6696aab1a5f765782bbcd096903954c9178054cb
ajax9309 <alexzomboe@gmail.com>
2023-03-23 01:47:01 -0300

    add appvue

--- scripts/genCheckBoxs.js
+++ scripts/genCheckBoxs.js
@@ -11,8 +11,8 @@ async function genCheckBoxs(tag_id) {
         category = category.map(each => {
             return `
                 <div class="p-1 pe-3">
-                <label class="text-primary" for="${each}">${each}</label>
-                <input onClick="capData()" class="checkbox ps-2" type="checkbox" name="${each}" value="${each}" id="${each}">
+                    <label class="text-primary" for="${each}">${each}</label>
+                    <input onClick="capData()" class="checkbox ps-2" type="checkbox" name="${each}" value="${each}" id="${each}">
                 </div>
              `
         })
